c6e68d7d742f0d30c3fd835833a45ca7596d38d6
BizzArt <102154976+HugoLorenzo@users.noreply.github.com>
2022-10-26 14:52:09 -0300

    First push

---
+++ js/App.js
@@ -0,0 +1,63 @@
+const showText = () => {
+    const text = document.getElementById('description').style.display = "block"
+}
+
+const hideText = () => {
+    const text = document.getElementById('description').style.display = "none"
+}
+
+const showText2 = () => {
+    const text = document.getElementById('description2').style.display = "block"
+}
+
+const hideText2 = () => {
+    const text = document.getElementById('description2').style.display = "none"
+}
+
+const showText3 = () => {
+    const text = document.getElementById('description3').style.display = "block"
+}
+
+const hideText3 = () => {
+    const text = document.getElementById('description3').style.display = "none"
+}
+
+const showText4 = () => {
+    const text = document.getElementById('description4').style.display = "block"
+}
+
+const hideText4 = () => {
+    const text = document.getElementById('description4').style.display = "none"
+}
+
+const showText5 = () => {
+    const text = document.getElementById('description5').style.display = "block"
+}
+
+const hideText5 = () => {
+    const text = document.getElementById('description5').style.display = "none"
+}
+
+const showText6 = () => {
+    const text = document.getElementById('description6').style.display = "block"
+}
+
+const hideText6 = () => {
+    const text = document.getElementById('description6').style.display = "none"
+}
+
+const showText7 = () => {
+    const text = document.getElementById('description7').style.display = "block"
+}
+
+const hideText7 = () => {
+    const text = document.getElementById('description7').style.display = "none"
+}
+
+const showText8 = () => {
+    const text = document.getElementById('description8').style.display = "block"
+}
+
+const hideText8 = () => {
+    const text = document.getElementById('description8').style.display = "none"
+}
